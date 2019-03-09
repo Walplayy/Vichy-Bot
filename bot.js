db752bd89fa38a8fd883af11ef44abc257a92891
Walplayy <48372620+Walplayy@users.noreply.github.com>
2019-03-08 17:45:57 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,5 @@ const token = require("./token.json").token;
 const bot = new discord.Client({
   disableEveryone: true
 });
-require("./lib/functions")(bot);
-
-bot.commands = new discord.Collection();
-bot.aliases = new discord.Collection();
-bot.afk = new Map();
-
-module.exports.bot = bot;
 
 bot.login(token);
